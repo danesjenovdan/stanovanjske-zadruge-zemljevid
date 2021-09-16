937151f79281e4a0487bffe0c9343c04c54c0248
patricijab <patricija.brecko@gmail.com>
2021-09-16 18:00:39 +0200

    kubernetes

--- stanovanjskeZadrugeZemljevidApi/stanovanjskeZadrugeZemljevidApi/settings.py
+++ stanovanjskeZadrugeZemljevidApi/stanovanjskeZadrugeZemljevidApi/settings.py
@@ -136,6 +136,23 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
-]
+# static files
+STATIC_ROOT = os.getenv('DJANGO_STATIC_ROOT', BASE_DIR / 'static')
+STATIC_URL = os.getenv('DJANGO_STATIC_URL_BASE', '/static/')
+
+# DJANGO STORAGE SETTINGS
+if os.getenv('DJANGO_ENABLE_S3', False):
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID', '<TODO>')
+    AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY', '<TODO>')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME', 'djnd')
+    AWS_DEFAULT_ACL = 'public-read' # if files are not public they won't show up for end users
+    AWS_QUERYSTRING_AUTH = False # query strings expire and don't play nice with the cache
+    AWS_LOCATION = os.getenv('DJANGO_AWS_LOCATION', 'stanovanjske-zadruge-zemljevid')
+    AWS_S3_REGION_NAME = os.getenv('DJANGO_AWS_REGION_NAME', 'fr-par')
+    AWS_S3_ENDPOINT_URL = os.getenv('DJANGO_AWS_S3_ENDPOINT_URL', 'https://s3.fr-par.scw.cloud')
+    AWS_S3_SIGNATURE_VERSION = os.getenv('DJANGO_AWS_S3_SIGNATURE_VERSION', 's3v4')
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
